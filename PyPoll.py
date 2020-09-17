63c4cc5cab9c507155e1274f167ed1937b6edf98
Delany <62618896+delanybroome@users.noreply.github.com>
2020-09-17 08:55:57 -0700

    commit issue

--- PyPoll.py
+++ PyPoll.py
@@ -1,7 +1,7 @@
 import os 
 import csv
 
-csv = os.path.join("C:/Users/delanybroome/Documents/GitHub/Python-Homework-Week-3","election_data.csv")
+csv = os.path.join("Documents", "GitHub" "Python-Homework-Week-3","election_data.csv")
 
 #Define Variables
 totalvotecount = 0
@@ -36,19 +36,34 @@ with open(csv) as csv_file:
     elif row[2] == canidate_list[3]:
         OTooley_Vote_Count = OTooley_Vote_Count + 1
 
+#calculate percentage of votes recieved
+Kahn_Percentage = ("%.2f" % (Kahn_Vote_Count/totalvotecount*100))
+Li_Percentage = ("%.2f" % (Li_Vote_Count/totalvotecount*100))
+Correy_Percentage = ("%.2f" % (Correy_Vote_Count/totalvotecount*100))
+OTooley_Percentage = ("%.2f" % (OTooley_Vote_Count/totalvotecount*100))
+
+#winner 
+if max(Kahn_Vote_Count, Correy_Vote_Count, Li_Vote_Count, OTooley_Vote_Count) == Kahn_Vote_Count:
+    winner = 'Kahn'
+elif max(Kahn_Vote_Count, Correy_Vote_Count, Li_Vote_Count, OTooley_Vote_Count) == Correy_Vote_Count:
+    winner = 'Correy'
+elif max(Kahn_Vote_Count, Correy_Vote_Count, Li_Vote_Count, OTooley_Vote_Count) == Li_Vote_Count:
+    winner = "Li"
+else: 
+    winner = "O'Tooley"
+
 #print 
-#print ("Election Results")
-#print ("---------------------------")
-#print (f"Total Votes: {totalvotecount}")
-#print ("---------------------------")
-#print(f"Kahn: {Kahn_Percentage}% {Kahn_Vote_Count}")
-#print(f"Correy: {Correy_Percentage}% {Correy_Vote_Count}")
-#print(f"Li: {Li_Percentage}% {Li_Vote_Count}")
-#print (f"O'Tooley: {OTooley_Percentage}% {OTooley_Vote_Count}")
-#print ("---------------------------")
-#print (f"Winner: {winner}")
-#print ("---------------------------")
+print ("Election Results")
+print ("---------------------------")
+print (f"Total Votes: {totalvotecount}")
+print ("---------------------------")
+print(f"Kahn: {Kahn_Percentage}% {Kahn_Vote_Count}")
+print(f"Correy: {Correy_Percentage}% {Correy_Vote_Count}")
+print(f"Li: {Li_Percentage}% {Li_Vote_Count}")
+print (f"O'Tooley: {OTooley_Percentage}% {OTooley_Vote_Count}")
+print ("---------------------------")
+print (f"Winner: {winner}")
+print ("---------------------------")
 
 #print list 
 #print(output)
-
